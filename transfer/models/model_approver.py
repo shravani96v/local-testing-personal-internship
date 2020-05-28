cf0a44415bd72c5c77b653186ef9e2640b5a0117
shravani96v <sv1059@wildcats.unh.edu>
2020-05-27 20:22:14 -0400

    testing the object replacement with name

--- transfer/models/model_approver.py
+++ transfer/models/model_approver.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+
 class Approver(models.Model):
     approver_id = models.AutoField(primary_key=True)
     approver_name = models.CharField(max_length=200, blank=True, null=True)
+
+
+def __str__(self):
+    return self.text[:50]
